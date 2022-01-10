926424feed77bac9a7b61083219195b8470de0d6
Kamil Guliyev <94452586+kamilquliyev98@users.noreply.github.com>
2022-01-11 01:53:13 +0400

    Console.WriteLine's

--- Market/Program.cs
+++ Market/Program.cs
@@ -9,19 +9,41 @@ namespace Market
         {
             CashRegister operation = new CashRegister(500, Enums.Currency.AZN, Enums.PaymentType.Cash);
 
-            operation.AddNewSale(80, Enums.Currency.AZN);
-            operation.AddNewSale(100, Enums.Currency.TRY);
-            operation.AddNewSale(100, Enums.Currency.USD);
-            operation.AddNewSale(100, Enums.Currency.EUR);
-
-            operation.RemoveSale(80, Enums.Currency.AZN);
-            operation.RemoveSale(100, Enums.Currency.TRY);
-            operation.RemoveSale(100, Enums.Currency.USD);
-            operation.RemoveSale(100, Enums.Currency.EUR);
-
-            operation.AddNewSale(55, Enums.Currency.AZN);
-
+            operation.AddNewSale(100, Enums.Currency.AZN);
             Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.AddNewSale(100, Enums.Currency.TRY);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.AddNewSale(100, Enums.Currency.USD);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.AddNewSale(100, Enums.Currency.EUR);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.RemoveSale(100, Enums.Currency.AZN);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.RemoveSale(100, Enums.Currency.TRY);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.RemoveSale(100, Enums.Currency.USD);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.RemoveSale(100, Enums.Currency.EUR);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.AddNewSale(10, Enums.Currency.USD);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
         }
     }
 }
